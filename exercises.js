6532e994afeb7ecf73283406f2b9999cd78e676c
Erwins Saget <erwinssaget@gmail.com>
2021-01-29 11:21:40 -0500

    Initial commit

---
+++ exercises.js
@@ -0,0 +1,77 @@
+function isRealPalindrome(str) {
+  // Your code here...
+}
+
+function runningTotal(arr) {
+  // Your code here...
+}
+
+function swap(sentence) {
+  // Your code here...
+}
+
+function wordSizes(sentence) {
+  // Your code here...
+}
+
+function union(arr1, arr2) {
+  // Your code here...
+}
+
+function firstRecurring(word) {
+  // Your code here...
+}
+
+function showMultiplicativeAverage(arr) {
+  // Your code here...
+}
+
+function multiplyList(arr1, arr2) {
+  // Your code here...
+}
+
+function sequence(num) {
+  // Your code here...
+}
+
+function wordCap(sentence) {
+  // Your code here...
+}
+
+function processReleaseData(newReleases) {
+  // Your code here...
+}
+
+function octalToDecimal(numberString) {
+  // Your code here...
+}
+
+function anagram(word, array) {
+  // Your code here...
+}
+
+function triangle(a, b, c) {
+  // Your code here...
+}
+
+function fridayThe13ths(year) {
+  // Your code here...
+}
+
+module.exports = {
+  isRealPalindrome,
+  runningTotal,
+  swap,
+  wordSizes,
+  union,
+  firstRecurring,
+  showMultiplicativeAverage,
+  multiplyList,
+  sequence,
+  wordCap,
+  processReleaseData,
+  octalToDecimal,
+  anagram,
+  triangle,
+  fridayThe13ths,
+};
